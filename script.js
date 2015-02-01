f994a71d0c662dee3c27d787f3bde866a163f238
Kimalye <kimalye@gmail.com>
2015-02-01 13:16:45 -0800

    Averages calculate properly now

--- script.js
+++ script.js
@@ -22,123 +22,121 @@ app.config(function ($routeProvider) {
 // 	});
 // };
 
+// app.directive('quiz', function(quizQuestions) {
+//   return {
+//     restrict: 'AE',
+//     scope: {},
+
+//     link: function(scope, elem, attrs) {
+//       scope.start = function() {
+//         scope.id = 0;
+//         scope.quizOver = false;
+//         scope.inProgress = true;
+//         scope.getQuestion();
+//       };
 
 
-app.directive('quiz', function(quizQuestions) {
-  return {
-    restrict: 'AE',
-    scope: {},
+//       scope.reset = function() {
+//         scope.inProgress = false;
+//         // scope.score = 0;
+//       };
 
-    link: function(scope, elem, attrs) {
-      scope.start = function() {
-        scope.id = 0;
-        scope.quizOver = false;
-        scope.inProgress = true;
-        scope.getQuestion();
-      };
-
-
-      scope.reset = function() {
-        scope.inProgress = false;
-        // scope.score = 0;
-      };
-
-      scope.getQuestion = function() {
-        var q = quizQuestions.getQuestion(scope.id);
-        if(q) {
-          scope.question = q.question;
-          scope.options = q.options;
-          scope.answer = q.answer;
-        } else {
-          scope.quizOver = true;
-        }
-      };
-
-
-
-      scope.class="blue";
-      scope.changeClass = function(copyCurrentSum) {
-        if (copyCurrentSum <= .47) {
-          scope.class = "red";
-          console.log("Happy");
-        } else if (copyCurrentSum >= .85) {
-          scope.class = "blue";
-          console.log("Sad");
-        } else {
-          scope.class = "green";
-          console.log("Neutral");
-        }
-      };
-
-      // scope.nextQuestion1 = function() {
-      //   console.log('hi');
-      //   };
-
-      var currentSum = 0;
-      //var oldAns = 0;
-      scope.nextQuestion = function() {
-        console.log('hi');
-
-        if(!$('input[name=answer]:checked').length) return;
-
-        var ans = $('input[name=answer]:checked').val();
+//       scope.getQuestion = function() {
+//         var q = quizQuestions.getQuestion(scope.id);
+//         if(q) {
+//           scope.question = q.question;
+//           scope.options = q.options;
+//           scope.answer = q.answer;
+//         } else {
+//           scope.quizOver = true;
+//         }
+//       };
 
 
 
-        console.log("Last answer: " + ans);
-        currentSum = currentSum + parseInt(ans);
-        //console.log("This is the current sum of numbers: " + currentSum);
+//       scope.class="blue";
+//       scope.changeClass = function(copyCurrentSum) {
+//         if (copyCurrentSum <= .47) {
+//           scope.class = "red";
+//           console.log("Happy");
+//         } else if (copyCurrentSum >= .85) {
+//           scope.class = "blue";
+//           console.log("Sad");
+//         } else {
+//           scope.class = "green";
+//           console.log("Neutral");
+//         }
+//       };
 
-        scope.id++;
+//       // scope.nextQuestion1 = function() {
+//       //   console.log('hi');
+//       //   };
 
-        var currentQuestion = scope.id;
-        console.log("--We are on question " + currentQuestion + "--");
-        var copyCurrentSum = currentSum;
+//       var currentSum = 0;
+//       //var oldAns = 0;
+//       $scope.nextQuestion = function() {
+//         console.log('hi');
 
-        //I'm sorry this is really huge, but it needs to divide by a
-        //certain number based on what question you are on.
+//         if(!$('input[name=answer]:checked').length) return;
 
-        if(currentQuestion == 1){
+//         var ans = $('input[name=answer]:checked').val();
 
-          console.log("Total sum: "+ copyCurrentSum +" out of 3");
-          copyCurrentSum = copyCurrentSum/3
-          console.log("divided by 3 is "+ copyCurrentSum);
-        };
 
-        if(currentQuestion == 2){
 
-          console.log("Total sum: "+ copyCurrentSum +" out of 6");
-          copyCurrentSum = copyCurrentSum/6
-          console.log("divided by 6 is "+ copyCurrentSum);
-        };
+//         console.log("Last answer: " + ans);
+//         currentSum = currentSum + parseInt(ans);
+//         //console.log("This is the current sum of numbers: " + currentSum);
 
-        if(currentQuestion == 3){
+//         scope.id++;
 
-          console.log("Total sum: "+ copyCurrentSum +" out of 9");
-          copyCurrentSum = copyCurrentSum/9
-          console.log("divided by 12 is "+ copyCurrentSum);
-        };
+//         var currentQuestion = scope.id;
+//         console.log("--We are on question " + currentQuestion + "--");
+//         var copyCurrentSum = currentSum;
 
-        if(currentQuestion == 4){
+//         //I'm sorry this is really huge, but it needs to divide by a
+//         //certain number based on what question you are on.
 
-          console.log("Total sum: "+ copyCurrentSum +" out of 12");
-          copyCurrentSum = copyCurrentSum/12
-          console.log("divided by 12 is "+ copyCurrentSum);
-        };
+//         if(currentQuestion == 1){
 
-        if(currentQuestion == 5){
+//           console.log("Total sum: "+ copyCurrentSum +" out of 3");
+//           copyCurrentSum = copyCurrentSum/3
+//           console.log("divided by 3 is "+ copyCurrentSum);
+//         };
 
-          console.log("Total sum: "+ copyCurrentSum +" out of 15");
-          copyCurrentSum = copyCurrentSum/15
-          console.log("divided by 12 is "+ copyCurrentSum);
-        };
+//         if(currentQuestion == 2){
 
-        scope.getQuestion();
-        scope.changeClass(copyCurrentSum);
+//           console.log("Total sum: "+ copyCurrentSum +" out of 6");
+//           copyCurrentSum = copyCurrentSum/6
+//           console.log("divided by 6 is "+ copyCurrentSum);
+//         };
 
-      }
+//         if(currentQuestion == 3){
 
-      scope.reset();
-    }
-  }
-});
+//           console.log("Total sum: "+ copyCurrentSum +" out of 9");
+//           copyCurrentSum = copyCurrentSum/9
+//           console.log("divided by 12 is "+ copyCurrentSum);
+//         };
+
+//         if(currentQuestion == 4){
+
+//           console.log("Total sum: "+ copyCurrentSum +" out of 12");
+//           copyCurrentSum = copyCurrentSum/12
+//           console.log("divided by 12 is "+ copyCurrentSum);
+//         };
+
+//         if(currentQuestion == 5){
+
+//           console.log("Total sum: "+ copyCurrentSum +" out of 15");
+//           copyCurrentSum = copyCurrentSum/15
+//           console.log("divided by 12 is "+ copyCurrentSum);
+//         };
+
+//         scope.getQuestion();
+//         scope.changeClass(copyCurrentSum);
+
+//       }
+
+//       scope.reset();
+//     }
+//   }
+// });
